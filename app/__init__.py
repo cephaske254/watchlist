f3f1f42cc5006d21c4a0b485214b579bde9da224
cephaske254 <cephaske254@gmail.com>
2020-04-30 01:04:43 +0300

    final

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+def create_app(config_name):
+    # Initializing application
+    app = Flask(__name__)
+
+    # Setting up configuration
+    app.config.from_object(config_options[config_name])
+
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ app/views.py
@@ -0,0 +1,7 @@
+# from flask import render_template, request,redirect,url_for
+# from app import app
+# from .request import get_movies, get_movie, search_movie
+
+# from .models import reviews
+# from .forms import ReviewForm
+# Review = reviews.Review
